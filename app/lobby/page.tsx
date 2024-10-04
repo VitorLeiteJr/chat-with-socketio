692df595d1938bb6706efa498206305c6acd1a80
VitorLeiteJr <96vitorleite@gmail.com>
2024-10-04 16:08:46 -0300

    feat: add ui and form to create room

--- app/lobby/page.tsx
+++ app/lobby/page.tsx
@@ -1,61 +1,103 @@
 'use client'
-import React, {  useEffect, useState } from 'react'
+import React, {  FormEvent, useEffect, useState } from 'react'
 import io,{ Socket } from 'socket.io-client';
+import ModalComp from '../components/modal';
 
 let socket: Socket;
 
 const Lobby: React.FC = () => {
 
-    const [room,setRoom] = useState<string>("");
     const [rooms,setRooms] = useState<string[]>([]);
+    const [isOpenModal,setIsOpenModal]  = useState<boolean>(false);
 
 
     useEffect(()=>{
         socket= io();      
         //socket.emit('getRooms');
    
-         // Listen for the room list from the server
-    socket.on('roomList', (rooms: string[]) => {
-      setRooms(rooms);
-      console.log(rooms);
-    });
+          // Listen for the room list from the server
+      socket.on('roomList', (rooms: string[]) => {
+        setRooms(rooms);
+        console.log(rooms);
+      });
 
-    // Request the room list when component loads
-    socket.emit('getRooms');
+      // Request the room list when component loads
+      socket.emit('getRooms');
 
 
       return () => {
         socket.disconnect();
       };
 
-    },[room]);
+    },[]);
 
 
-    const createRoom = () =>{
-     socket.emit("joinRoom",room);
-      //setRoom("");
+    const createRoom = (e: FormEvent<HTMLFormElement>) =>{
+      e.preventDefault();
+      const formData = new FormData(e.currentTarget);
+      const nameRoom = formData.get("roomName") as string;
 
+      socket.emit("joinRoom",nameRoom);
   };
+
+    const handleModalClose = () => {
+      setIsOpenModal(false);
+    }
 
 
   return (
-    <div className=''>
-        List of Rooms
+      <div>
        
-        <input value={room}onChange={(e)=>setRoom(e.target.value)}className="border-l-pink-500"></input>
-        <button onClick={createRoom} className="border-spacing-3 bg-red-300" >CREATE ROOM</button>
-        
-        <div>
-        {rooms.map((list,index)=>(
-             
-             <div key={index}>{list}
-                 <button className='bg-gray-200'>join</button>
-             </div>
-   
-        ))}
-             </div>
-        
-    </div>
+        <div className="min-h-screen flex-1 bg-gray-200 p-4 flex justify-center items-center">
+        {isOpenModal ? (
+          <ModalComp
+          handleCreateRoom={createRoom}
+          handleModalClose={handleModalClose}
+          />
+        ) : <></>}
+          
+              <div className="bg-white w-full md:max-w-4xl rounded-lg shadow">
+              <div className="h-12 flex justify-between items-center border-b border-gray-200 m-4">
+                <div >
+                <div className="text-xl font-bold text-gray-700">Lobby</div>
+                <div className="text-sm font-base text-gray-500">List of rooms</div>
+                </div>
+              </div>
+              <div className="px-6">
+
+                {rooms.map((rooms,index)=>(
+                       <div key={index} className="flex justify-between items-center h-16 p-4 my-6  rounded-lg border border-gray-100 shadow-md">
+                       <div className="flex items-center">
+                         <img className="rounded-full h-12 w-12" src="https://static-cdn.jtvnw.net/jtv_user_pictures/27fdad08-a2c2-4e0b-8983-448c39519643-profile_image-70x70.png" alt="Logo" />
+                       <div className="ml-2">
+                         <div className="text-sm font-semibold text-gray-600">{rooms}</div>
+                         <div className="text-sm font-light text-gray-500">Level 6 - Warlock</div>
+                       </div>
+                       </div>
+                       <div>
+                         <button className="bg-red-400 hover:bg-red-500 p-2 rounded-full shadow-md flex justify-center items-center">
+                           <svg className="text-white toggle__lock w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+                           </svg>
+                         </button>
+                       </div>
+                     </div>
+                ))}                
+
+                <div className="flex bg-gray-200 justify-center items-center h-16 p-4 my-6  rounded-lg  shadow-inner">
+                  <div className="flex items-center border border-gray-400 p-2 border-dashed rounded cursor-pointer">
+                  <div>
+                    <svg className="text-gray-500 w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
+                  </svg>
+                  </div>
+                    <div onClick={()=>setIsOpenModal(true)}className="ml-1 text-gray-500 font-medium">Create a room</div>
+                  </div>
+                </div>
+              </div>
+              </div>
+    </div></div>
+
   )
 }
 
--- server.js
+++ server.js
@@ -9,18 +9,44 @@ const port = 3001;
 const app = next({ dev, hostname, port });
 const handler = app.getRequestHandler();
 
+
 app.prepare().then(() => {
   const httpServer = createServer(handler);
-
   const io = new Server(httpServer);
 
   io.on("connection", (socket) => {
-   console.log("a user connected: ",socket.id);
+ // console.log("a user connected: ",socket.id);
+  
+ const getActiveRooms = () => {
+  const rooms = [...io.sockets.adapter.rooms.keys()].filter(
+    (room) => !io.sockets.adapter.sids.get(room)
+  );
+  return rooms;
+};
+
+// Send the list of rooms to all connected clients
+const emitRooms = () => {
+  const rooms = getActiveRooms();
+  io.emit('roomList', rooms); // Broadcast room list to all clients
+};
+
+// Send the updated room list whenever a new client requests it
+socket.on('getRooms', () => {
+  emitRooms(); // Send the rooms to all clients
+});
+
+// Join a specific room and then broadcast the updated room list
+socket.on('joinRoom', (room) => {
+  socket.join(room);
+  console.log(`User with ID ${socket.id} joined room: ${room}`);
+  emitRooms(); // Broadcast updated room list after join
+});
 
    socket.on("sendMessage", (message)=>{
-    io?.emit("receiveMessage", message);
+    io.emit("receiveMessage", message);
    })
   });
+
 
   httpServer
     .once("error", (err) => {
